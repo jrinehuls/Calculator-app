9863bb6db249b9fc1f9cfbe99063d706c912f2e4
Justin Rinehuls <jrinehuls@gmail.com>
2022-11-05 16:03:07 -0400

    finish button functionality and update button colors

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let display = "Calculator Output";
-document.getElementById("display-label").innerHTML = display;
-
-
-
-
+let topDisplay = "";
+let bottomDisplay = "0";
+let result = "";
+let reset = true;
+document.getElementById("bottom-label").innerHTML = bottomDisplay;
+const numButtons = document.querySelectorAll(".num-button");
+const aritButtons = document.querySelectorAll(".arit-button");
+const clearButton = document.getElementById("clear");
+const recipButton = document.getElementById("recip");
+const expButton = document.getElementById("exp");
+const squareButton = document.getElementById("square");
+const sqrtButton = document.getElementById("sqrt");
+const negButton = document.getElementById("neg");
+const equalsButton = document.getElementById("equals");
 
 
 //-----------------The basic operations------------------
@@ -27,21 +35,8 @@ function modulus(a, b) {
     return a % b;
 }
 
-function recip(a) {
-    if (a != 0) {
-        return 1/a;
-    }
-    else {
-        return "Cannot divide by zero!"
-    }
-}
-
-function sqrt(a) {
-    return Math.sqrt(a);
-}
-
-function square(a) {
-    return Math.pow(a,2);
+function powers(a, b) {
+    return Math.pow(a, b);
 }
 
 //--------------Switch for choosing operation-------------
@@ -62,8 +57,166 @@ function onEquals(operation, a, b) {
         case '%':
             return modulus(a, b);
             break;
+        case '^':
+            return powers(a, b);
+            break;
         default:
-            return "0ver 9000!";
+            return "Over 9000!";
             break;
        }
 }
+
+//----------------Some non-math functions----------------
+function updateLowerDisplay(bottomDisplay) {
+    document.getElementById("bottom-label").innerHTML = bottomDisplay;
+}
+
+function updateUpperDisplay(topDisplay) {
+    document.getElementById("top-label").innerHTML = topDisplay;
+}
+
+//---------------------------Add event listeners---------------------
+/* Clicking a number button adds to the bottom display. Also checks if
+"reset" is true (at start or because "=" was pushed) to reset top label
+and clear out bottom label.
+*/
+numButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (reset) {
+            topDisplay = "";
+            updateUpperDisplay(topDisplay);
+            bottomDisplay = "";
+            reset = false;
+        }
+        bottomDisplay+=button.innerHTML;
+        updateLowerDisplay(bottomDisplay);
+    })
+});
+//--Clicking an operand add to string and moves up to upper display--
+aritButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (bottomDisplay != "") {
+            topDisplay = bottomDisplay;
+            topDisplay +=` ${button.innerHTML} `;
+            updateUpperDisplay(topDisplay);
+            bottomDisplay = "";
+            updateLowerDisplay(bottomDisplay);
+            reset = false;
+        }
+    })
+});
+//------------------event listener for recip button-------------------
+function recip() {
+    reset=true;
+    if (bottomDisplay != "") {
+        bottomDisplay = onEquals('^', bottomDisplay, -1);
+        updateLowerDisplay(bottomDisplay);
+    }    
+};
+
+recipButton.addEventListener('click', recip);
+//---------------event listener for exponent button-------------------
+function exponent() {
+    if (bottomDisplay != "") {
+        topDisplay = bottomDisplay;
+        topDisplay += ' ^ ';
+        updateUpperDisplay(topDisplay);
+        bottomDisplay = "";
+        updateLowerDisplay(bottomDisplay);
+        reset = false;
+    }   
+};
+
+expButton.addEventListener('click', exponent)
+
+//---------------event listener for square button--------------------
+function square() {
+    reset=true;
+    if (bottomDisplay != "") {
+        bottomDisplay = onEquals('^', bottomDisplay, 2);
+        updateLowerDisplay(bottomDisplay);
+    }    
+};
+
+squareButton.addEventListener('click', square)
+//---------------event listener for square root button------------------
+function squareRoot() {
+    reset=true;
+    if (bottomDisplay != "") {
+        bottomDisplay = onEquals('^', bottomDisplay, 0.5);
+        updateLowerDisplay(bottomDisplay);
+    }    
+};
+
+sqrtButton.addEventListener('click', squareRoot)
+//---------------event listener for negation button------------------
+function negate() {
+    reset=true;
+    if (bottomDisplay != "") {
+        bottomDisplay = 0 - Number(bottomDisplay);
+        updateLowerDisplay(bottomDisplay);
+    }    
+};
+
+negButton.addEventListener('click', negate)
+//---------------event listener for clear button------------------
+function clear() {
+    reset=true;
+    updateUpperDisplay('');
+    updateLowerDisplay(0);  
+};
+
+clearButton.addEventListener('click', clear)
+//-------------Click equals button to display result-----------------
+
+function calculate() {
+    let split = topDisplay.split(" ");
+    if (split.length == 3) {
+        a = Number(split[0]);
+        operation = split[1];
+        b = Number(bottomDisplay);
+        topDisplay = `${topDisplay} ${bottomDisplay} =`;
+        bottomDisplay = onEquals(operation,a,b);
+        updateUpperDisplay(topDisplay);
+        updateLowerDisplay(bottomDisplay);
+        reset = true;
+    }
+};
+
+equalsButton.addEventListener('click', calculate);
+
+//---Checks for numeric key and pressed to add to lower display-----
+window.addEventListener("keydown", function (e) {
+    numButtons.forEach(button => {
+        if(button.innerHTML===e.key) {
+            if (reset) {
+                topDisplay = "";
+                updateUpperDisplay(topDisplay);
+                bottomDisplay = "";
+                reset = false;
+            }
+            bottomDisplay+=button.innerHTML;
+            updateLowerDisplay(bottomDisplay);
+        }
+    })
+    aritButtons.forEach(button => {
+        if(button.innerHTML===e.key) {
+            if (bottomDisplay != "") {
+                topDisplay = bottomDisplay;
+                topDisplay+=` ${button.innerHTML} `;
+                updateUpperDisplay(topDisplay);
+                bottomDisplay="";
+                updateLowerDisplay(bottomDisplay);
+                reset = false;
+            }
+        }
+    })
+    if(e.key === '^') {
+        exponent();
+    }
+
+    if(e.key === '=' || e.key === "Enter") {
+        calculate();
+    }
+    //--------need to do with squares and stuff--------------
+});
